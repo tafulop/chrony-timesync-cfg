dfb78f986768cc7417da43c3b1888604e44d0756
Tamas <tamas.flp@gmail.com>
2021-06-11 19:37:01 +0200

    Switch to briged network

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm --network host --privileged --name chrony time-server:latest bash -c 'service chrony start && date -s 2021-12-31 &&watch -n 1.0 date'
+docker run -it --rm --privileged --name chrony time-server:latest bash -c 'service chrony start && date -s 2021-12-31 &&watch -n 1.0 date'
